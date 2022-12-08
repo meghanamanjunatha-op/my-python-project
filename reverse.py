1c3f5be552950e206d44c8c80f3d2c25ae09fd33
meghana <meghana.mgowda13@gmail.com>
2022-12-07 22:38:13 -0600

    first commit

---
+++ reverse.py
@@ -0,0 +1,16 @@
+def rev_string(string):
+    return string[::-1]
+
+
+org_string = "my name is megs"
+print(rev_string(org_string))
+
+
+""""
+original string
+def reverse_string(string):
+    return string[::-1]
+
+a = "lawrahK namA"
+print(reverse_string(a))
+"""
---
+++ validatepalindrome.py
@@ -0,0 +1,2 @@
+def ValidatePalindrome(x):
+
